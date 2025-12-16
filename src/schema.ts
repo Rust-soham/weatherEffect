fc0cd6a374075acbd421300bfa1889b1457a6cba
Rust-soham <sohambhattacharjee2002@gmail.com>
2025-12-16 12:35:35 +0000

    finished schema

--- src/schema.ts
+++ src/schema.ts
@@ -2,11 +2,37 @@ import { Schema } from "effect";
 
 export class Weather extends Schema.Class<Weather>("Weather")({
     name: Schema.String,
+
     coord: Schema.Struct({
         lon: Schema.Number,
         lat: Schema.Number
     }),
-    
 
+    weather: Schema.NonEmptyArrayEnsure(
+        Schema.Struct({
+            main: Schema.String,
+            description: Schema.String
+        })
+    ),
+
+    main: Schema.Struct({
+        temp: Schema.Number,
+        feelsLike: Schema.Number,
+        tempMin: Schema.Number,
+        tempMax: Schema.Number,
+        humidity: Schema.Number,
+        pressure: Schema.Number
+    }),
+    
+    wind: Schema.Struct({
+        speed: Schema.Number,
+        deg: Schema.Number,
+    }),
+
+    sys: Schema.Struct({
+        country: Schema.String
+    })
 
 }) {}
+
+
